2a4abc8e742708380fe1d0d7b1324724f338c958
Bentot23 <92287648+Bentot23@users.noreply.github.com>
2023-03-13 02:04:05 -0500

    hit 75 mins

---
+++ app/controllers/instructors_controller.rb
@@ -0,0 +1,45 @@
+class InstructorsController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :invalid
+rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+    def index
+        instructors = Instructor.all
+        render json: instructors, include: :students, status: :ok
+    end
+
+    def show
+        instructor = Instructor.find(params[:id])
+        render json: instructor, status: :ok
+    end
+
+    def create
+        instructor = Instructor.create!(instructor_params)
+        render json: instructor, status: :created
+    end
+
+    def update
+        instructor = Instructor.find(params[:id])
+        instructor.update!(instructor_params)
+        render json: instructor, status: :ok
+    end
+
+    def destroy
+        instructor = Instructor.find(params[:id])
+        instructor.destroy
+        head :no_content
+    end
+
+    private
+
+    def instructor_params
+        params.permit(:name)
+    end
+
+    def not_found
+        render json: {"error": "Instructor not found"}
+    end
+
+    def invalid
+        render json: {"errors": ["validation errors"]}
+    end
+end
---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,46 @@
+class StudentsController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :invalid
+rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+    def index
+        students = Student.all
+        render json: students, status: :ok
+    end
+
+    def show
+        student = Student.find(params[:id])
+        render json: student, status: :ok
+    end
+
+    def create
+        student = Student.create!(student_params)
+        render json: student, status: :created
+    end
+
+    def update
+        student = Student.find(params[:id])
+        student.update!(student_params)
+        render json: student, status: :ok
+    end
+
+    def destroy
+        student = Student.find(params[:id])
+        student.destroy
+        head :no_content
+    end
+
+    private
+
+    def student_params
+        params.permit(:name, :major, :age)
+    end
+
+    def not_found
+        render json: {"error": "Instructor not found"}
+    end
+
+    def invalid
+        render json: {"errors": ["validation errors"]}
+    end
+    
+end
